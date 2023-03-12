0efe70857ca14b12c8ef2366b899413fef1c233c
judemaij <judemaij@gmail.com>
2023-03-12 06:33:10 +0000

    Update at 2023-03-12T06:33:10.487927Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,19 +2,17 @@ package com.bytelegend;
 
 public class Challenge {
     public static int sum = 0;
-
     public static void main(String[] args) {
-        addAverageOf(1, 3);
+        addAverageOf(9, 11);
         System.out.println("addAverageOf(1,3): " + sum);
-        addAverageOf(2, 6);
+        addAverageOf(0, 0);
         System.out.println("addAverageOf(2,6): " + sum);
-        addAverageOf(3, 5);
+        addAverageOf(1, 3);
         System.out.println("addAverageOf(3,5): " + sum);
     }
-
     public static void addAverageOf(int a, int b) {
-        sum = a + b;
-        int average = sum / 2;
-        sum = sum + average;
+        int sumInside = a + b;
+        int average = sumInside / 2;
+        sum = average;
     }
 }
